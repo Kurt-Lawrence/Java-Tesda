64a5c0242f9589af56e09dc51c03769e4e3d2539
Kurt-Lawrence <156872026+Kurt-Lawrence@users.noreply.github.com>
2024-06-22 22:53:51 +0800

    Sum of n ... (n + 1) - n, which n is array of numbers

---
+++ Assignments/Cumulative_Sum/CumulativeSum.java
@@ -0,0 +1,24 @@
+package Assignments.Cumulative_Sum;
+
+public class CumulativeSum {
+    public static void main(String[] args) {
+
+        System.out.println("Cumulative Sum: " + cumulativeSum(5,2));
+        
+    }
+
+    static int cumulativeSum(int... nums){
+        
+        int sumOfAll = 0;   //variable for sum
+
+        
+        for(int num:nums){  //loop through the list of numbers
+            for(int x = num ; x > 0; x--){  //loop to descent through the number ex. 4,3,2,1
+                sumOfAll += x;
+            }
+            System.out.println();
+        }
+
+        return sumOfAll;
+    }
+}
