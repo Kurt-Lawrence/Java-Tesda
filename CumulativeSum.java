7767ae82836bd0935bfee89a77979b39bcb0ddf5
Kurt-Lawrence <156872026+Kurt-Lawrence@users.noreply.github.com>
2024-06-22 22:29:03 +0800

    Create CumulativeSum

---
+++ CumulativeSum.java
@@ -0,0 +1,16 @@
+
+public class Api{
+
+    public static void main(String[] args) {
+
+        cumulativeSum(5,2);
+        
+    }
+
+    static void cumulativeSum(int... nums){
+
+        for(int num:nums){
+            System.out.println(num);
+        }    
+    }
+}
