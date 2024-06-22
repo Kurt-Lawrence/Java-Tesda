00fb3746fc43ab1f418309f7502e4e3f17dea973
Kurt-Lawrence <156872026+Kurt-Lawrence@users.noreply.github.com>
2024-06-22 22:51:30 +0800

    Constructors on Books

---
+++ Assignments/Book_OOP/Book.java
@@ -0,0 +1,26 @@
+package Assignments.Book_OOP;
+
+public class Book {
+    String title;
+    String author;
+    int yearPublished;
+    double price;
+
+
+
+    public Book(String title, String author, int yearPublished, double price) {
+        this.title = title;
+        this.author = author;
+        this.yearPublished = yearPublished;
+        this.price = price;
+    }  
+
+    static void showDetails(Book book){
+
+        System.out.println("Title: " + book.title);
+        System.out.println("Author: " + book.author);
+        System.out.println("Year Published: " + book.yearPublished);
+        System.out.println("Price: " + book.price);
+    
+    }
+}
