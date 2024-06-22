b3dd9ac10e881d81cce5eba6bbf40bc675df9bd8
Kurt-Lawrence <156872026+Kurt-Lawrence@users.noreply.github.com>
2024-06-22 22:50:30 +0800

    Are you a palindrome?

---
+++ Programming_Challenges/Palindrome/Palindrome.java
@@ -0,0 +1,25 @@
+package Programming_Challenges.Palindrome;
+
+import java.util.Scanner;
+
+public class Palindrome {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter string: ");
+        String word = in.nextLine();
+
+        in.close();
+
+        StringBuilder sb_word = new StringBuilder(word);
+        
+        if(word.equals(sb_word.reverse().toString())){
+            System.out.println("Palindrome");
+        }else{
+            System.out.println("Not a Palindrome");
+        }
+        
+
+    }//main end
+}
