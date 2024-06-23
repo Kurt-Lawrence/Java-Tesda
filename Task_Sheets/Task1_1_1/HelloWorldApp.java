d3fa518e7499b08b28474f6f08f284ad19d0fe86
Kurt-Lawrence <156872026+Kurt-Lawrence@users.noreply.github.com>
2024-06-23 12:45:04 +0800

    Task 1-1-1

---
+++ Task_Sheets/Task1_1_1/HelloWorldApp.java
@@ -0,0 +1,13 @@
+package Task_Sheets.Task1_1_1;
+
+/**
+ * @author Kurt Lawrence
+ * @since 2024-06-23
+ */
+public class HelloWorldApp {
+    public static void main(String[] args) {
+
+        System.out.println("Kumusta Mundo!"); 
+
+    }
+}
