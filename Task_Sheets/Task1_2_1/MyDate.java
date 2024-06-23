8378eb81fd45489ed743eb0e02bd06054286f023
Kurt-Lawrence <156872026+Kurt-Lawrence@users.noreply.github.com>
2024-06-23 23:28:47 +0800

    Task 1-2-1

---
+++ Task_Sheets/Task1_2_1/MyDate.java
@@ -0,0 +1,13 @@
+package Task_Sheets.Task1_2_1;
+
+
+public class MyDate {
+    public static void main(String[] args) {
+        DateTask date1 = new DateTask(1,1,1978);
+        DateTask date2 = new DateTask(9,21,1984);
+        
+        System.out.println(date1.toString());
+        System.out.println(date2.toString());
+        DateTask.leapYears();// display leapyears
+    }
+}
