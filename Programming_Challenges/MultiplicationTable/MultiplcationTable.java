34d448dbdb0b9c07433802601462f4e3e01596e5
Kurt-Lawrence <156872026+Kurt-Lawrence@users.noreply.github.com>
2024-07-04 23:04:18 +0800

    Multiplication Table

---
+++ Programming_Challenges/MultiplicationTable/MultiplcationTable.java
@@ -0,0 +1,26 @@
+package Programming_Challenges.MultiplicationTable;
+
+import java.util.Scanner;
+
+public class MultiplcationTable {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter number for multiplication table: ");
+        multiplcationTable(in.nextInt());
+
+    }
+
+    static void multiplcationTable(int num){
+
+        for (int x = 1; x <= 10; x++){
+
+            System.out.println(x + " x " + num + " = " + x*num);
+
+        }
+
+    }
+    
+}
